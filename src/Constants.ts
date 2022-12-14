44ccd9c1fa62a0ec00dd6bd2148a7e4103184000
RicardoKd <richardo.kadyan@gmail.com>
2022-12-14 15:51:05 +0200

    fixed API sub urls

--- src/Constants.ts
+++ src/Constants.ts
@@ -3,12 +3,12 @@ const BASE_API_URL = 'https://bloggy-api.herokuapp.com';
 const API = Object.freeze({
   GET_POSTs: (postId = -1) =>
     BASE_API_URL + `/posts${postId === -1 ? '' : `/${postId}`}`,
-  CREATE_POST: () => BASE_API_URL,
+  CREATE_POST: () => BASE_API_URL + '/posts',
   DELETE_POST: (postId: number) => BASE_API_URL + `/posts/${postId}`,
   UPDATE_POST: (postId: number) => BASE_API_URL + `/posts/${postId}`,
   GET_COMMENTs: (postId = -1) =>
     BASE_API_URL + `/comments${postId === -1 ? '' : `?postId=${postId}`}`,
-  CREATE_COMMENT: () => BASE_API_URL,
+  CREATE_COMMENT: () => BASE_API_URL + '/comments',
 });
 
 const SLICE_NAMES = {
