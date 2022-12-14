48f33c8d3333675b663eb3374e0e7a8b67918d8c
RicardoKd <richardo.kadyan@gmail.com>
2022-12-14 22:25:55 +0200

    routing v1.1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { ROUTES } from './Constants';
+import CreatePost from './pages/CreatePost';
 import EditPost from './pages/EditPost';
 import Home from './pages/Home';
+import ViewPost from './pages/ViewPost';
 
 const App = () => {
   return (
     <Routes>
       <Route path={ROUTES.GENERAL} element={<Home />} />
       <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.CREATE_POST} element={<CreatePost />} />
+      <Route path={ROUTES.VIEW_POST} element={<ViewPost />} />
       <Route path={ROUTES.EDIT_POST} element={<EditPost />} />
     </Routes>
   );
