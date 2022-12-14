375e2d8b3f5369b8b3a7823a7576896afcff7e48
RicardoKd <richardo.kadyan@gmail.com>
2022-12-14 13:48:31 +0200

    components v1.0.0 header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import './App.css';
+import Header from './components';
 
 const App = () => {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Header></Header>
+  </div>;
 };
 
 export default App;
---
+++ src/components/index.tsx
@@ -0,0 +1,26 @@
+import { useAppDispatch } from '../app/Hooks';
+import { createPost } from '../reducers/PostsReducer';
+
+const Header = () => {
+  const dispatch = useAppDispatch();
+
+  const date = new Date(Date.now());
+
+  const fullDate = `Today: ${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`;
+
+  const createPostBtnClick = () => {
+    void dispatch(createPost({ title: 'This is title', body: 'This is body' }));
+  };
+
+  return (
+    <header>
+      <p>{fullDate}</p>
+      <h1>Your Blog</h1>
+      <button type="button" onClick={() => createPostBtnClick()}>
+        Create post
+      </button>
+    </header>
+  );
+};
+
+export default Header;
